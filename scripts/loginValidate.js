6ca1945bc57816646c93998b870a4f18f668611f
Fabiona Tafciu <ftafciu21@epoka.edu.al>
2023-06-02 00:50:59 +0200

    Finished validation of log in

--- scripts/loginValidate.js
+++ scripts/loginValidate.js
@@ -4,8 +4,7 @@ function validateForm() {
       alert("Username must be filled out");
       return false;
     }
-    
-    password = ((?=^.{6,}$)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9]).*);
+      
     let y = document.forms["login"]["pasword"].value;
     if (y == "") {
         alert("Password must be filled out");
