277d7723e6380b90433d16d132ccd24e814fc672
unknown <you@example.com>
2023-06-05 19:24:43 +0200

    Working on validation

--- scripts/loginValidate.js
+++ scripts/loginValidate.js
@@ -1,14 +1,13 @@
 function validateForm() {
-    let x = document.forms["login"]["username"].value;
-    if (x == "") {
-      alert("Username must be filled out");
-      return false;
-    }
-    
-    password = ((?=^.{6,}$)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9]).*);
-    let y = document.forms["login"]["pasword"].value;
-    if (y == "") {
-        alert("Password must be filled out");
-        return false;
-      } 
+  let x = document.forms["login"]["username"].value;
+  if (x == "") {
+    alert("Username must be filled out");
+    return false;
   }
+    
+  let y = document.forms["login"]["pasword"].value;
+  if (y == "") {
+      alert("Password must be filled out");
+      return false;
+    } 
+}
