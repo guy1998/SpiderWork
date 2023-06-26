7cc1b93fc717ca4a57c8546ccb65b9efecf0a8e5
guy1998 <acifliku21@epoka.edu.al>
2023-06-26 14:15:28 +0200

    Fixed menu in employer view

--- views/employer.php
+++ views/employer.php
@@ -221,10 +221,8 @@ $current_emp = fetchEmployer($_SESSION['userid']);
             <li><button type="button" onclick="dropdownOptions()"></button></li>
             <div id="optionsMenu">
                 <ul id="optionLinks">
-                    <li><a href="../views/profileCustomization.php" onclick="redirectToProfileChange()">My account</a>
-                    </li>
-                    <li><a href="">Settings</a></li>
-                    <li><a href="../controller/logOutController.php">Log out</a></li>
+                    <li id="customize"><a href="../views/profileCustomization.php">My account</a></li>
+                    <li id="logOut"><a href="../controller/logOutController.php">Log out</a></li>
                 </ul>
             </div>
 
